fc9349f8804123bf1d6ba1e932e6ec3de0fcfe8e
stevenabreu7 <s.abreu@rug.nl>
2025-03-06 12:11:01 -0800

    torch patch for checkpoint loading

---
+++ torch_patch.sh
@@ -0,0 +1,2 @@
+# cp venv/lib/python3.10/site-packages/torch/distributed/checkpoint/state_dict.py venv/lib/python3.10/site-packages/torch/distributed/checkpoint/state_dict_orig.py
+cp torch.distributed.checkpoint.state_dict.py venv/lib/python3.10/site-packages/torch/distributed/checkpoint/state_dict.py
